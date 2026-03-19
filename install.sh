59926a38bbefef2b2d4bc850859584cc08ffd065
Igack <legoae86@gmail.com>
2026-03-19 12:08:52 -0400

    Fix install script for existing installs

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-git clone https://github.com/HyunLee8/SpotPitch.git && cd SpotPitch && bash SpotPitch.command
+
+if [ -d "SpotPitch" ]; then
+    echo "SpotPitch already installed, updating..."
+    cd SpotPitch && git pull && bash SpotPitch.command
+else
+    git clone https://github.com/HyunLee8/SpotPitch.git && cd SpotPitch && bash SpotPitch.command
+fi
