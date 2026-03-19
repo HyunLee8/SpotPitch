501078ba4553661003705b10197a047ccc23b202
Igack <legoae86@gmail.com>
2026-03-19 12:11:20 -0400

    One line for both install and launch

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ -d "SpotPitch" ]; then
-    echo "SpotPitch already installed, updating..."
     cd SpotPitch && git pull && bash SpotPitch.command
 else
     git clone https://github.com/HyunLee8/SpotPitch.git && cd SpotPitch && bash SpotPitch.command
